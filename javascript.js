07b788e29f9d8df232c1090e5adeb18dbf8adb40
jcampbell <jcampbell57@gmail.com>
2022-06-07 20:51:06 -0700

    implemented grid generator

---
+++ javascript.js
@@ -0,0 +1,19 @@
+
+//default
+let size = 13;
+let cells = Math.pow(size,2);
+const sketch = document.querySelector('.sketch');
+const columns = document.querySelector('.columns');
+const rows = document.querySelector('.rows');
+
+
+function generate(cells) {
+    sketch.style.setProperty('--grid-rows', size);
+    sketch.style.setProperty('--grid-cols', size);
+    for (i=0; i<cells; i++) {
+        let div = document.createElement('div');
+        sketch.appendChild(div).className = 'cell';
+    } 
+}
+
+generate(cells);
