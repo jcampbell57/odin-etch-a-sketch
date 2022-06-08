1440ffc7cdf4b2daf4bf421eb657d66441e36226
jcampbell <jcampbell57@gmail.com>
2022-06-07 21:16:50 -0700

    added css hover effect and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # odin-etch-a-sketch
+
+By the end of this project, I will be able to:
+- create a grid with javascript of any input size
+- implement a hover affect that gives each grid cell a random background color
--- javascript.js
+++ javascript.js
@@ -6,7 +6,7 @@ const sketch = document.querySelector('.sketch');
 const columns = document.querySelector('.columns');
 const rows = document.querySelector('.rows');
 
-
+//etch-a-sketch logic
 function generate(cells) {
     sketch.style.setProperty('--grid-rows', size);
     sketch.style.setProperty('--grid-cols', size);
@@ -17,3 +17,4 @@ function generate(cells) {
 }
 
 generate(cells);
+
