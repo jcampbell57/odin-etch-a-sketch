f6cfd9629879b96d33f540eb09b57533f5292360
jcampbell <jcampbell57@gmail.com>
2022-06-09 15:27:28 -0700

    sketchpad now functional with reset button and working size input

--- javascript.js
+++ javascript.js
@@ -1,20 +1,72 @@
 
 //default
-let size = 13;
-let cells = Math.pow(size,2);
-const sketch = document.querySelector('.sketch');
-const columns = document.querySelector('.columns');
-const rows = document.querySelector('.rows');
+let defaultSize = 16;
+const sketchpad = document.querySelector('.sketchpad');
 
-//etch-a-sketch logic
-function generate(cells) {
-    sketch.style.setProperty('--grid-rows', size);
-    sketch.style.setProperty('--grid-cols', size);
+//generate grid with css grid
+//let cells = Math.pow(size,2);
+/* function generate(cells) {
+    sketchpad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    sketchpad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     for (i=0; i<cells; i++) {
         let div = document.createElement('div');
-        sketch.appendChild(div).className = 'cell';
+        sketchpad.appendChild(div).className = 'cell';
     } 
+} */
+
+//generate grid inline-block
+function generate(size) {
+
+    //clear previous grid
+    let rows = sketchpad.querySelectorAll('div');
+    rows.forEach((div) => div.remove());
+
+    //generate grid
+    for (let i=0; i<size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        sketchpad.appendChild(row)
+
+        for (let i=0; i<size; i++) {
+            const div = document.createElement('div');
+            div.classList.add('cell');
+            div.style.width = `${500 / size}px`;
+            div.style.height = `${500 / size}px`;
+            row.appendChild(div);
+        }   
+    }
+};
+
+//mouseover color change
+sketchpad.addEventListener('mouseover', changeColor);
+function changeColor(e) {
+    let cell = e.target;
+    let red = Math.floor((Math.random() * 256) +1);
+    let green = Math.floor((Math.random() * 256) +1);
+    let blue = Math.floor((Math.random() * 256) +1);
+    return cell.style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
+/* cell.addEventListener('mouseenter', (e) => {
+    console.log(e)
+    cell.style.backgroundColor = 'darkred'
+})     */
+
+
+//reset board
+function reset() {
+    generate(defaultSize)
 }
 
-generate(cells);
+
+//change cell size with input 
+function changeSize(input) {
+    if (input > 1 && input < 101) {
+        generate(input);
+    } else {
+        console.log('error!')
+    }
+
+}
+
+generate(defaultSize);
 
